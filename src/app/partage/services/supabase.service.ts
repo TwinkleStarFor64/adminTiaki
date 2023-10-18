2ebc8db77e6582af6d952585611a0ac73fa7620a
Xavier64 <xavierpincon@gmail.com>
2023-10-18 14:58:43 +0200

    feat users.service et comment method

--- src/app/partage/services/supabase.service.ts
+++ src/app/partage/services/supabase.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import {
   AuthSession,
   createClient,
-  SupabaseClient
+  SupabaseClient,
 } from '@supabase/supabase-js';
 import { environment } from 'src/environments/environement';
 import { RoleData, UtilisateurData } from '../modeles/Types';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupabaseService {
   private supabase: SupabaseClient; // Instance du client Supabase
@@ -20,52 +20,58 @@ export class SupabaseService {
   user: any; // Utilisé dans la méthode signIn()
   token!: string; // Utilisé dans la méthode signIn()
 
-  constructor(private router: Router) { this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey) }
+  constructor(private router: Router) {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
 
   // Connexion à l'application - Tuto : https://www.youtube.com/watch?v=hPI8OegHPYc
-  
+
   signIn(email: string, password: string) {
-     this.supabase.auth.signInWithPassword({ email, password })
-     .then((res) => {
-      console.log(res); 
-      this.user = res.data.user; // La réponse de la méthode avec toutes les données d'un utilisateur
-      console.log("L'id de l'utilisateur authentifié : ",this.user.id);
-      
-      if (res.data.user!.role === 'authenticated') { // Je vérifie que le rôle et 'authenticated' dans supabase - voir le résultat de console.log(res)
-        this.token = res.data.session!.access_token; // Je stock la valeur du token retourné par supabase
-      if (this.token) {
-        sessionStorage.setItem('token', this.token); // set du token de session           
-      }
-        this.router.navigate(['intranet']);
-      }       
-    })
-    .catch((err) => {
-      console.log(err);           
-    });
+    this.supabase.auth
+      .signInWithPassword({ email, password })
+      .then((res) => {
+        console.log(res);
+        this.user = res.data.user; // La réponse de la méthode avec toutes les données d'un utilisateur
+        console.log("L'id de l'utilisateur authentifié : ", this.user.id);
+
+        if (res.data.user!.role === 'authenticated') {
+          // Je vérifie que le rôle et 'authenticated' dans supabase - voir le résultat de console.log(res)
+          this.token = res.data.session!.access_token; // Je stock la valeur du token retourné par supabase
+          if (this.token) {
+            sessionStorage.setItem('token', this.token); // set du token de session
+          }
+          this.router.navigate(['intranet']);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   // Récupérer les utilisateurs sur la table public.utilisateur
   async getUtilisateur() {
     const data = await this.supabase.from('utilisateur').select('*');
-    console.log("Méthode getUtilisateur", data);
+    console.log('Méthode getUtilisateur', data);
     return data;
   }
 
   // Récupérer les utilisateurs sur la table auth.users (table d'authentification de supabase)
   async listUser() {
     const response = await this.supabase.auth.admin.listUsers();
-    console.log("Méthode listUser - response.data.users", response.data.users);
-    return response.data.users; // Retournez les données des utilisateurs  
+    console.log('Méthode listUser - response.data.users', response.data.users);
+    return response.data.users; // Retournez les données des utilisateurs
   }
 
-
-/*
-*Supprimer utilisateur
-*/
+  /*
+   *Supprimer utilisateur
+   */
   async deleteUser(userData: string) {
-    const { data, error } = await this.supabase.auth.admin.deleteUser(userData); 
+    const { data, error } = await this.supabase.auth.admin.deleteUser(userData);
     if (data) {
-      console.log("suppression réussie");
+      console.log('suppression réussie');
     }
     if (error) {
       console.log(error);
@@ -77,14 +83,16 @@ export class SupabaseService {
     try {
       const { data } = await this.supabase.from('roles').select('*');
       if (data) {
-        console.log("Méthode getRoles - Données récupérées :", data);
+        console.log('Méthode getRoles - Données récupérées :', data);
         return data;
       } else {
         throw new Error("Aucune donnée n'a été récupérée pour les rôles.");
       }
     } catch (error) {
-      console.error("Erreur lors de la récupération des rôles :", error);
-      throw new Error("Impossible de récupérer les rôles. Veuillez consulter les logs pour plus d'informations.");
+      console.error('Erreur lors de la récupération des rôles :', error);
+      throw new Error(
+        "Impossible de récupérer les rôles. Veuillez consulter les logs pour plus d'informations."
+      );
     }
   }
 
@@ -93,25 +101,26 @@ export class SupabaseService {
     const { data, error } = await this.supabase
       .from('attribuerRoles')
       .select('idRole');
-    if (data) console.log("Méthode fetchAttribuerRoles", data);
+    if (data) console.log('Méthode fetchAttribuerRoles', data);
     if (error) console.log(error);
   }
-
-
 
   // Méthode pour récupérer les utilisateurs et leur rôles
   async getAllUsersWithRoles(): Promise<UtilisateurData[]> {
     // Récupérez uniquement l'email et le nom des utilisateurs depuis la table utilisateur.
-    const { data: utilisateursData, error: utilisateursError } = await this.supabase
-      .from('utilisateur')
-      .select('id,  email, nom'); // Incluez le champ "id" pour l'utilisateur.
+    const { data: utilisateursData, error: utilisateursError } =
+      await this.supabase.from('utilisateur').select('id,  email, nom'); // Incluez le champ "id" pour l'utilisateur.
 
     if (utilisateursError) {
-      console.error('Erreur lors de la récupération des utilisateurs :', utilisateursError);
+      console.error(
+        'Erreur lors de la récupération des utilisateurs :',
+        utilisateursError
+      );
     }
 
     // Récupérez les rôles associés à chaque utilisateur depuis la table attribuerRoles.
-    for (const utilisateur of utilisateursData as UtilisateurData[]) { // Utilisez le type correct - interface UtilisateurData
+    for (const utilisateur of utilisateursData as UtilisateurData[]) {
+      // Utilisez le type correct - interface UtilisateurData
       const idUtilisateur = utilisateur.id; // Contient l'id des utilisateurs de la table utilisateur
       //console.log("idUtilisateur", idUtilisateur);
 
@@ -119,27 +128,31 @@ export class SupabaseService {
         .from('attribuerRoles')
         .select('idRole')
         .eq('idUtilisateur', idUtilisateur); // Comparaison de l'id de la table avec l'id de la variable
-        // rolesData contient la ForeignKey idRole de la table attribuerRoles
-        //console.log("rolesData", rolesData);
+      // rolesData contient la ForeignKey idRole de la table attribuerRoles
+      //console.log("rolesData", rolesData);
 
       if (rolesError) {
-        console.error('Erreur lors de la récupération des rôles de l\'utilisateur :', rolesError);
+        console.error(
+          "Erreur lors de la récupération des rôles de l'utilisateur :",
+          rolesError
+        );
         continue;
       }
 
       // Récupérez les détails des rôles depuis la table des rôles.
-      const idRoles = rolesData.map(entry => entry.idRole);
+      const idRoles = rolesData.map((entry) => entry.idRole);
       // map sur rolesData afin de récupérer toutes les ForeignKeys
       //console.log("idRoles", idRoles);
-      const { data: rolesDetailsData, error: rolesDetailsError } = await this.supabase
-        .from('roles')
-        .select('*')
-        .in('id', idRoles); // Comparaison de la ForeignKey avec les id de la table roles
-        // rolesDetailsData contient les résultats de la comparaison au dessus
-        //console.log(rolesDetailsData);        
+      const { data: rolesDetailsData, error: rolesDetailsError } =
+        await this.supabase.from('roles').select('*').in('id', idRoles); // Comparaison de la ForeignKey avec les id de la table roles
+      // rolesDetailsData contient les résultats de la comparaison au dessus
+      //console.log(rolesDetailsData);
 
       if (rolesDetailsError) {
-        console.error('Erreur lors de la récupération des détails des rôles :', rolesDetailsError);
+        console.error(
+          'Erreur lors de la récupération des détails des rôles :',
+          rolesDetailsError
+        );
         continue;
       }
 
@@ -150,86 +163,40 @@ export class SupabaseService {
     // Les utilisateurs avec email, nom et rôles sont maintenant dans utilisateursData.
     //console.log('Utilisateurs avec email, nom et rôles :', utilisateursData);
     // Pour afficher les rôles, vous pouvez utiliser une boucle ou une fonction map.
-    for (const utilisateur of utilisateursData as UtilisateurData[]) { // Utilisez le type correct - interface RoleData
-      const roles = utilisateur.roles.map((nomRole: RoleData) => nomRole.role).join(', ');
+    for (const utilisateur of utilisateursData as UtilisateurData[]) {
+      // Utilisez le type correct - interface RoleData
+      const roles = utilisateur.roles
+        .map((nomRole: RoleData) => nomRole.role)
+        .join(', ');
       console.log(`Rôles de ${utilisateur.nom}: ${roles}`);
     }
     return utilisateursData as UtilisateurData[];
   }
 
-
-
-  /* async getUserById(id: string) {
-    const { data, error } = await this.supabase.auth.admin.getUserById(id);
-    if (data) {
-      console.log("getUserById fonction ", data);
-    }
-    if (error) {
-      console.log(error);
-    }
-  } */
-
   async getLoggedInUser() {
-    const { data: { user } } = await this.supabase.auth.getUser()
-    console.log( "Méthode getLoggedInUser : ", user );
-    return user;    
+    const {
+      data: { user },
+    } = await this.supabase.auth.getUser();
+    console.log('Méthode getLoggedInUser : ', user);
+    return user;
   }
 
   // Récupérer les utilisateurs sur la table public.utilisateur
   async getUtilisateurById(id: string) {
     const data = await this.supabase
-    .from('utilisateur')
-    .select('*')
-    .eq('id', id);
-    console.log("Méthode getUtilisateurById", data);
+      .from('utilisateur')
+      .select('*')
+      .eq('id', id);
+    console.log('Méthode getUtilisateurById', data);
     return data;
   }
-
-  
-
-
-
-
-
 }
-
-
-
-
-// '3845e166-dd29-47b4-924a-de0a1e50d454' Denver
-// 'b3108489-62d7-45d4-8e02-0096dc12b78b' Toto
-
-
-
-
-//   async attribuerRolesAUtilisateur(idUtilisateur: string, idRolesAAttribuer: string[]) {
-//     try {
-//       // Boucle à travers la liste d'ID de rôles et créez une entrée pour chaque rôle attribué
-//       for (const idRole of idRolesAAttribuer) {
-//         // Insérez une nouvelle entrée dans la table attribuerRoles
-//         const { data, error } = await this.supabase
-//           .from('attribuerRoles')
-//           .upsert([
-//             {
-//               idUtilisateur: idUtilisateur,
-//               idRole: idRole
-//             }
-//           ]);
-        
-//         if (error) {
-//           console.error('Erreur lors de l\'attribution du rôle à l\'utilisateur :', error);
-//           // Gérez l'erreur comme vous le souhaitez
-//         }
-//       }
-//     } catch (error) {
-//       console.error('Erreur lors de l\'attribution des rôles à l\'utilisateur :', error);
-//       // Gérez l'erreur comme vous le souhaitez
-//     }
-//   }
-
-/*
-*Récupérer
-*/
-
-
-
+/* async getUserById(id: string) {
+  const { data, error } = await this.supabase.auth.admin.getUserById(id);
+  if (data) {
+    console.log("getUserById fonction ", data);
+  }
+  if (error) {
+    console.log(error);
+  }
+} */
---
+++ src/app/partage/services/users.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+import { RoleData, UtilisateurData, UtilisateurI } from '../modeles/Types';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  allUsersData: UtilisateurData[] = [];
+  utilisateur: UtilisateurI[] = [];
+  authUsers: UtilisateurI[] = [];
+  selectedUsers: UtilisateurData[] = [];
+
+  constructor(public supa:SupabaseService) { }
+
+async fetchUtilisateur() {
+    const { data, error } = await this.supa.getUtilisateur();
+    if (data) {
+      this.utilisateur = data.map((item: { [x: string]: any }) => ({
+        id: item['id'],
+        email: item['email'],
+        nom: item['nom']
+      }));
+      console.log("Méthode fetchUtilisateur", this.utilisateur.map((item) => item['id']).join(', '));
+    }
+    if (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchAuthUsers() {
+    try {
+      const userData = await this.supa.listUser();
+      if (userData) {
+        this.authUsers = userData.map((item: { [x: string]: any }) => ({
+          id: item['id'],
+          email: item['email'],
+          nom: item['nom']
+        }));
+        console.log("Méthode fetchAuthUsers : ", this.authUsers.map((item) => item['email']).join(', '));
+      } else {
+        throw new Error("Aucune donnée utilisateur disponible.");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la récupération des utilisateurs:", error);
+      throw new Error("Echec de la méthode fetchAuthUsers. Veuillez consulter les logs pour plus d'informations.");
+    }
+  }
+
+  async fetchAllUsersWithRoles() {
+    try {
+      const usersWithRolesData: UtilisateurData[] = await this.supa.getAllUsersWithRoles();
+      if (usersWithRolesData !== undefined) { // Vérification de nullité
+        this.allUsersData = usersWithRolesData.map((item) => ({
+          id: item.id,
+          email: item.email,
+          nom: item.nom,
+          roles: item.roles,
+          selected:false,
+        }));
+
+        // Afficher les utilisateurs avec leurs emails et rôles dans la console
+        this.allUsersData.forEach((user) => {
+          const allRoles = user.roles.map((role: RoleData) => role.role).join(', ');
+          console.log(`Nom: ${user.nom}, Email: ${user.email}, Rôles: ${allRoles}`);
+        });
+        return;
+
+      } else {
+        throw new Error('Aucune donnée utilisateur et rôles disponibles');
+      }
+    } catch (error) {
+      console.error("Erreur lors de la récupération des données rôles et utilisateurs", error);
+      throw new Error("Echec de la méthode fetchAllUsersWithRoles. Veuillez consulter les logs pour plus d'informations.");
+    }
+  }
+}
