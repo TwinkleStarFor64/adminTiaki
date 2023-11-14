da71b9c7c15c675fa8e6f53a941c13821ebbf696
DavidFer64 <davidfer6415@gmail.com>
2023-11-14 10:31:55 +0100

    feat style css personnalisé des boutons PrimeNG-V2

--- src/app/partage/services/donnees.service.spec.ts
+++ src/app/partage/services/donnees.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DonneesService } from './donnees.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DonneesService', () => {
   let service: DonneesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(DonneesService);
   });
 
