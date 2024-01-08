3e81517d3f3b42da4df081c468a22e103d50b4bf
Xavier64 <xavierpincon@gmail.com>
2024-01-08 08:57:50 +0100

    push dev test ok

--- src/app/intranet/template/dialog/ajout-menu/ajout-menu.component.spec.ts
+++ src/app/intranet/template/dialog/ajout-menu/ajout-menu.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AjoutMenuComponent } from './ajout-menu.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenusPipe } from 'src/app/pipes/menus.pipe';
+import { PlatsPipe } from 'src/app/pipes/plats.pipe';
 
 describe('AjoutMenuComponent', () => {
   let component: AjoutMenuComponent;
@@ -8,7 +14,9 @@ describe('AjoutMenuComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AjoutMenuComponent]
+      declarations: [AjoutMenuComponent,MenusPipe,PlatsPipe],
+      imports: [ReactiveFormsModule,FormsModule,ButtonModule,NgxPaginationModule],
+      providers: [DynamicDialogRef]
     });
     fixture = TestBed.createComponent(AjoutMenuComponent);
     component = fixture.componentInstance;
