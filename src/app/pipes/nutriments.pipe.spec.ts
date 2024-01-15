adc14b1b79a9a97386ff7d980941b87c84dce9cb
Xavier64 <xavierpincon@gmail.com>
2024-01-15 10:54:28 +0100

    feat merge branch DT 561

--- src/app/pipes/nutriments.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NutrimentsPipe } from './nutriments.pipe';
-
-describe('NutrimentsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NutrimentsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
